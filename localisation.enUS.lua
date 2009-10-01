f74d83a3f47953d5d63a5b927275bb5accb038b5
Tjark Saul <tjark@saul.li>
2009-10-01 21:15:06 +0000

    English localization update

--- localisation.enUS.lua
+++ localisation.enUS.lua
@@ -6,6 +6,17 @@ if L then
 	L['Added X DKP to player Y.'] = function(X,Y)
 		return 'Added ' .. X .. ' DKP for player ' .. Y .. '.';
 	end;
-	L["Avoidance: "] = true;
+	L["Armor"] = "Armor"
+	L["ArmorDmgReduce"] = "Percent of damage reduced by armor"
+	L["Avoidance"] = "Avoidance"
+	L["Avoidance: "] = "Avoidance: "
+	L["Block"] = "Block"
+	L["BlockValue"] = "Block Value"
+	L["Dodge"] = "Dodge"
+	L["Eternal Earthstorm Diamond"] = "Eternal Earthstorm Diamond"
+	L["HP"] = "Life"
+	L["Parry"] = "Parry"
+	L["Shield Mastery"] = "Shield Mastery"
+	L["Shield Specialization"] = "Shield Specialization"
 
 end;
