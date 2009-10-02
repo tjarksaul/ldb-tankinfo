0ad4588c7204b584efe3c59d1bd30a74651726e9
Tjark Saul <tjark@saul.li>
2009-10-02 17:52:24 +0000

    added tooltip updated some formulas

--- core.lua
+++ core.lua
@@ -2,12 +2,37 @@
 
 TankInfo = LibStub("AceAddon-3.0"):NewAddon("Broker_TankInfo", "AceConsole-3.0", "AceTimer-3.0", "AceEvent-3.0");
 local L = LibStub("AceLocale-3.0"):GetLocale("TankInfo");
+local TankInfo = TankInfo
 
--- creating ldb data source
-local dataobj = ldb:NewDataObject("tankinfo", {type = "data source", text = L["Avoidance: "].."UNKNOWN"})
+-- creating ldb data object
+local doTable = {
+	type = "data source", 
+	text = '|c0066ff00'..L["Avoidance: "]..'|r|c00ff0000'.."UNKNOWN|r", 
+	label = 'Broker TankInfo', 
+	icon = "Interface\\Icons\\Ability_Warrior_DefensiveStance"
+}
+local dataobj = ldb:NewDataObject("tankinfo", doTable)
 
 -- creating variables
-local level, bosslvl, dodge, block, parry, blockvalue, armor, health, eh, avoidance, enemymiss, mitigation, enemycrit, armorDR;
+local level, bosslvl;
+local enemymiss, dodge, block, parry;
+local blockvalue;
+local armor, health, eh, armorDR;
+local avoidance, mitigation;
+local enemycrit, normalhit;
+local defaults;
+
+-- default values for db
+do
+	defaults = {
+		char = {
+			bosslvl = UnitLevel('player') + 3,
+			r1, g1, b1 = 0,0.4,1,
+			r2, g2, b2 = 0,1,0,
+			enabled = true;
+		}
+	}
+end;
 
 --[[
 function TankInfo:TankInfoSlash(input)
@@ -30,13 +55,12 @@ function TankInfo:getBlockval()
   end
 
   bval_unmodified = bval
-  if gem and gem == L['Eternal Earthstorm Diamond'] then
-    bval_mod = bval_mod + 0.1
+  if gem and (gem == L['Eternal Earthstorm Diamond'] or gem == L['Eternal Earthsiege Diamond']) then
+    bval_mod = bval_mod + 0.05
   end
 
   -- get block value modifier in talents
   local numTabs = GetNumTalentTabs();
-  local _, clss = UnitClass("player")
   for t=1, numTabs do
     local numTalents = GetNumTalents(t);
     for i=1, numTalents do
@@ -45,7 +69,7 @@ function TankInfo:getBlockval()
         if clss == "SHAMAN" then
           bval_mod = bval_mod + currRank * 5 / 100 -- rank is 1..5, values are 5%, 10%, 15%, 20%, 25%
         else
-          bval_mod = bval_mod + currRank / 10 -- rank is 1, 2, 3, values are: 10%, 20%, 30% => / 10 is equivalent to * 10 / 100
+          bval_mod = bval_mod + currRank * 15 / 100 -- rank is 1, 2 values are: 15%, 30%
         end
 
         break
@@ -78,14 +102,14 @@ function TankInfo:Values()
 	bosslvl = level + 3;
 	dodge = GetDodgeChance();
 	parry = GetParryChance();
-	block = GetBlockChance();
+	block = floor(GetBlockChance() * 100) / 100;
 	blockvalue = self:getBlockval();
 	enemymiss = self:getBasicMiss();
 	armor = self:getArmor();
 	if (bosslvl < 60) then
 		armorDR = armor / (armor + 400 + 85 * bosslvl);
 	else
-		armorDR = armor / (armor + 400 + 85 * (bosslvl + 4.5 * (bosslvl - 59)));
+		armorDR = (armor / ((467.5 * bosslvl) + armor - 22167.5));
 	end;
 	health = UnitHealth("player");
 	eh = floor(health / (1 - armorDR));
@@ -96,11 +120,12 @@ function TankInfo:Values()
 	else
 		enemycrit = 5.6 - self:getCritReduce();
 	end;
+	normalhit = 100 - mitigation;
 end;
 
 function TankInfo:UpdateLDB()
 	self:Values();
-	dataobj.text = L["Avoidance: "]..avoidance.."%";
+	dataobj.text = '|c000066ff'..L["Avoidance: "]..'|r|c0000ff00'..avoidance.."%|r";
 end;
 
 function TankInfo:Aura(unit)
@@ -114,10 +139,46 @@ function TankInfo:levelup(arg1)
 	self:UpdateLDB();
 end;
 
+-- what will be printed in the tooltip
+function TankInfo:OnTooltipShow()
+	self:AddLine(L['Defense Stats'],1,1,1);
+	self:AddDoubleLine(L['Enemymiss']..': ',(enemymiss + 5)..'%',0,0.4,1,0,0.4,1); -- adds enemymiss line
+	self:AddDoubleLine(L['Dodge']..': ',floor(dodge * 100) / 100 ..'%',0,1,0,0,1,0); -- adds dodge line
+	self:AddDoubleLine(L['Parry']..': ',floor(parry * 100) / 100 ..'%',0,0.4,1,0,0.4,1); -- adds parry line
+	self:AddDoubleLine(L['Avoidance']..': ',avoidance..'%',0,1,0,0,1,0); -- adds avoidance line
+	self:AddDoubleLine(L['dwAvoid']..': ',(avoidance + 24)..'%',0,0.4,1,0,0.4,1); -- adds dw avoidance line
+	self:AddDoubleLine(L['Block Chance']..': ',block..'%',0,1,0,0,1,0); -- adds block chance line
+	self:AddDoubleLine(L['Block Value']..': ',blockvalue,0,0.4,1,0,0.4,1); -- adds block value line
+	self:AddDoubleLine(L['Mitigation']..': ',mitigation..'%',0,1,0,0,1,0); -- adds mitigation line
+	self:AddDoubleLine(L['dwMitigation']..': ',(mitigation + 24)..'%',0,0.4,1,0,0.4,1) -- adds dw mitigation line
+	self:AddDoubleLine(L['NormalHit']..': ',normalhit..'%',0,1,0,0,1,0); -- adds line for chance to get a normal hit
+	self:AddDoubleLine(L['GetCrit']..': ',enemycrit..'%',0,0.4,1,0,0.4,1); -- adds line for chance to get critically hit
+	self:AddLine(' '); -- adds empty line
+	self:AddDoubleLine(L['Health']..': ',health,0,0.4,1,0,0.4,1); -- adds health line
+	self:AddDoubleLine(L['ArmorDR']..': ',floor(armorDR * 10000) / 100 ..'%',0,1,0,0,1,0); -- adds line for damage reduction by armor
+	self:AddDoubleLine(L['EH']..': ',eh,0,0.4,1,0,0.4,1); -- adds line for effective health
+	self:AddLine(' '); -- adds empty line
+	self:AddLine(L['Enemy is level X'](bosslvl),1,1,1);
+end;
+
+-- happens when the mouse joins the ldb dataobject
+function dataobj:OnEnter()
+	GameTooltip:SetOwner(self, "ANCHOR_NONE");
+	GameTooltip:SetPoint("TOPLEFT", self, "BOTTOMLEFT");
+	GameTooltip:ClearLines();
+	TankInfo.OnTooltipShow(GameTooltip);
+	GameTooltip:Show();
+end;
+
+--happens when the mouse leaves the DO
+function dataobj:OnLeave()
+	GameTooltip:Hide();
+end;
+
 -- OnInitialize
 function TankInfo:OnInitialize()
 	self:Print('OnInitialize');
-	self.db = LibStub("AceDB-3.0"):New("MyAddonDB")
+	self.db = LibStub("AceDB-3.0"):New("TankInfoDB", defaults, true);
 end;
 
 -- OnEnable
@@ -131,6 +192,7 @@ function TankInfo:OnEnable()
 	self:Values();
 	self.updateInterval = self:ScheduleRepeatingTimer("UpdateLDB", 0.5);
 	self:UpdateLDB();
+	self.db.char.enabled = true;
 end;
 
 --OnDisable
@@ -139,4 +201,6 @@ function TankInfo:OnDisable()
 	self:UnregisterEvent("PLAYER_LEVEL_UP");
 	self:UnregisterEvent("UNIT_INVENTORY_CHANGED");
 	self:UnregisterEvent("UNIT_AURA");
-end;
+	self:CancelTimer(self.updateInterval);
+	self.db.char.enabled = false;
+	end;
--- localisation.enUS.lua
+++ localisation.enUS.lua
@@ -10,6 +10,7 @@ if L then
 	L["ArmorDmgReduce"] = "Percent of damage reduced by armor"
 	L["Avoidance"] = "Avoidance"
 	L["Avoidance: "] = "Avoidance: "
+	L["dwAvoid"] = 'Avoidance vs Dual Wield enemy';
 	L["Block"] = "Block"
 	L["BlockValue"] = "Block Value"
 	L["Dodge"] = "Dodge"
@@ -18,5 +19,20 @@ if L then
 	L["Parry"] = "Parry"
 	L["Shield Mastery"] = "Shield Mastery"
 	L["Shield Specialization"] = "Shield Specialization"
-
+	L["Enemy is level X"] = function(bosslevel)
+		if not bosslevel then return 'UNKNOWN' end;
+		return 'Enemy is level ' .. bosslevel .. '.';
+	end;
+	L['Defense Stats'] = true;
+	L['Enemymiss'] = 'The enemy\'s chance to miss you';
+	L['Block Chance'] = true;
+	L['Block Value'] = true;
+	L['Mitigation'] = true;
+	L['dwMitigation'] = 'Mitigation vs Dual Wield enemy';
+	L['NormalHit'] = 'Chance to receive a normal hit';
+	L['GetCrit'] = 'Chance to get critically hit';
+	L['Health'] = true;
+	L['ArmorDR'] = 'Damage reduction by armor';
+	L['EH'] = 'Effective Health';
+	L['Eternal Earthsiege Diamond'] = true;
 end;
